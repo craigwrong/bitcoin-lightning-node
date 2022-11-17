1d363e33f196bedacee9aa749fed5fc068066e6a
craigwrong <95532896+craigwrong@users.noreply.github.com>
2022-11-17 15:00:38 +0000

    REST API config fix.

--- scripts/start-lightning-api.sh
+++ scripts/start-lightning-api.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --name lightning-api -d --restart unless-stopped -e PROTOCOL=http -p 3001:3001 -p 4001:4001 -v $HOME/bitcoin-lightning-node/shares/dot-lightning:/root/.lightning -v $HOME/bitcoin-lightning-node/shares/lightning-api-certs:/usr/src/app/certs lightning-api
+docker run --name lightning-api -d --restart unless-stopped -e PROTOCOL=http -p 3001:3001 -p 4001:4001 -v $HOME/bitcoin-lightning-node/shares/dot-lightning:/root/.lightning -v $HOME/bitcoin-lightning-node/shares/lightning-api-certs:/root/c-lightning-REST/certs lightning-api
