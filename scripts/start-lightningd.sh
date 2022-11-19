44c08a03dee7a892f87fa3f895bc86f211479147
craigwrong <95532896+craigwrong@users.noreply.github.com>
2022-11-19 10:38:24 +0000

    Open port on main net script.

--- scripts/start-lightningd.sh
+++ scripts/start-lightningd.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --name lightningd -d --restart unless-stopped --network bitcoind --ip 10.1.0.3 -v $HOME/bitcoin-lightning-node/shares/dot-lightning:/root/.lightning -v /media/ln-backup:/ln-backup -v /TIMECHAIN:/root/.bitcoin:ro lightningd
+docker run --name lightningd -d --restart unless-stopped --network bitcoind --ip 10.1.0.3 -p 9735:9735 -v $HOME/bitcoin-lightning-node/shares/dot-lightning:/root/.lightning -v /media/ln-backup:/ln-backup -v /TIMECHAIN:/root/.bitcoin:ro lightningd
